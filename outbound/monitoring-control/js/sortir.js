bd5bb34cfe2527ed3b193ad1a67bb47ea3b25788
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-26 20:52:40 +0700

    Update sortir.js

--- outbound/monitoring-control/js/sortir.js
+++ outbound/monitoring-control/js/sortir.js
@@ -741,7 +741,7 @@ cancelLogoutBtn?.addEventListener("click", () => {
 });
 confirmLogoutBtn?.addEventListener("click", () => {
   localStorage.clear();
-  window.location.href = "monitoring-control/sort-job.html"; // ✅ BENAR
+  window.location.href = "../index.html"; // ✅ Dari /outbound/monitoring-control/ balik ke /outbound/index.html
 });
 window.addEventListener("click", (e) => {
   if (e.target === logoutModal) {
