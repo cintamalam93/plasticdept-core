7188e76976370326b6fae922f2debc282a03527d
aldiansyah9395 <aldiansyah93@outlook.com>
2025-06-24 09:32:45 +0700

    update

--- outbound/monitoring-control/js/sortir.js
+++ outbound/monitoring-control/js/sortir.js
@@ -1019,7 +1019,7 @@ function checkPythonAPIAvailable() {
     .catch(() => false);
 }
 
-async function saveOutJobAchievement( {
+async function saveOutJobAchievement() {
   // 1. Ambil semua job dari node PhxOutboundJobs
   const jobsSnap = await get(ref(db, "PhxOutboundJobs"));
   if (!jobsSnap.exists()) return;
